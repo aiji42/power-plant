134abfbfd7f0fdc5c5372dc38a980bf3f4e16516
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-17 22:12:35 +0900

    update(remix): update news page

--- remix/app/routes/news.tsx
+++ remix/app/routes/news.tsx
@@ -1,6 +1,14 @@
-import { LoaderFunction } from 'remix'
+import { LoaderFunction, useLoaderData } from 'remix'
 import { VFC } from 'react'
 import { parse } from 'node-html-parser'
+
+type Data = {
+  url: string
+  title: string
+  image: string
+  sample: string
+  actor: string
+}[]
 
 export const loader: LoaderFunction = async () => {
   const res = await fetch(
@@ -16,21 +24,32 @@ export const loader: LoaderFunction = async () => {
 
   const lists = root.querySelectorAll('.rank_list li')
 
-  console.log(
-    lists.map((node) => [
-      node.querySelector('a')?.getAttribute('href'),
-      node.querySelector('.title')?.innerText,
-      node.querySelector('img')?.getAttribute('src'),
-      node.querySelector('.button_sample')?.getAttribute('href'),
-      node.querySelector('.name a')?.innerText
-    ])
-  )
-
-  return null
+  return lists.map<Data[number]>((node) => ({
+    url: node.querySelector('a')?.getAttribute('href') ?? '',
+    title: node.querySelector('.title')?.innerText ?? '',
+    image: node.querySelector('img')?.getAttribute('src') ?? '',
+    sample: node.querySelector('.button_sample')?.getAttribute('href') ?? '',
+    actor: node.querySelector('.name a')?.innerText ?? ''
+  }))
 }
 
 const News: VFC = () => {
-  return null
+  const data = useLoaderData<Data>()
+
+  return (
+    <>
+      {data.map(({ url, title, image }) => (
+        <a href={url} key={url}>
+          <p>{title}</p>
+          <img
+            loading="lazy"
+            src={image.replace('pf_t1', 'pf_e')}
+            width="50%"
+          />
+        </a>
+      ))}
+    </>
+  )
 }
 
 export default News
