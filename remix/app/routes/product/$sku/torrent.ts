0441b287542ec03138ea66b6047289f576588f93
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-21 23:16:26 +0900

    update(remix): update torrent page cache

--- remix/app/routes/product/$sku/torrent.ts
+++ remix/app/routes/product/$sku/torrent.ts
@@ -1,4 +1,4 @@
-import { LoaderFunction } from 'remix'
+import { LoaderFunction, json } from 'remix'
 import parse from 'node-html-parser'
 
 export type Data = {
@@ -21,45 +21,57 @@ export const loader: LoaderFunction = async ({ params }) => {
 
   const dataList = await Promise.all(
     codes.map(async (q) => {
-      const res = await fetch(`${HOST}s/?q=${q}&f=0&c=0_0`)
-      const html = await res.text()
-      const root = parse(html)
-      const trs = root.querySelectorAll('tr.default')
-      return trs
-        .map((tr) => {
-          return tr
-            .querySelectorAll('td')
-            .slice(1)
-            .map((td) => {
-              const a = td.querySelector('a')
-              if (a) {
-                const href = a.getAttribute('href')
-                if (!href?.startsWith('/download')) return a.innerText
-                return href
+      try {
+        const res = await fetch(`${HOST}/?q=${q}&f=0&c=0_0`)
+        const html = await res.text()
+        const root = parse(html)
+        const trs = root.querySelectorAll('tr.default')
+        return trs
+          .map((tr) => {
+            return tr
+              .querySelectorAll('td')
+              .slice(1)
+              .map((td) => {
+                const a = td.querySelector('a')
+                if (a) {
+                  const href = a.getAttribute('href')
+                  if (!href?.startsWith('/download')) return a.innerText
+                  return href
+                }
+                return td.innerText
+              })
+          })
+          .reduce<Data>(
+            (
+              res,
+              [title, link, size, registeredAt, seeder, leech, completed]
+            ) => [
+              ...res,
+              {
+                title,
+                link: `${HOST}${link}`,
+                size,
+                registeredAt,
+                seeder,
+                leech,
+                completed
               }
-              return td.innerText
-            })
-        })
-        .reduce<Data>(
-          (
-            res,
-            [title, link, size, registeredAt, seeder, leech, completed]
-          ) => [
-            ...res,
-            {
-              title,
-              link: `${HOST}${link}`,
-              size,
-              registeredAt,
-              seeder,
-              leech,
-              completed
-            }
-          ],
-          []
-        )
+            ],
+            []
+          )
+      } catch (e) {
+        console.error(e)
+        return []
+      }
     })
   )
 
-  return { data: dataList.flat() }
+  return json(
+    { data: dataList.flat() },
+    {
+      headers: {
+        'cache-control': 'public, max-age=3600, stale-while-revalidate=3600'
+      }
+    }
+  )
 }
