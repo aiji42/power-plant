a1910a2d784b012a1184189e8b92c4e748fa9a7c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-24 23:55:05 +0900

    update(batch): update Dockerfile

---
+++ batch/script.ts
@@ -0,0 +1,64 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { spawn } from 'child_process'
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+
+const s3 = new S3Client({})
+
+const aria = async (target: string) =>
+  new Promise((resolve, reject) => {
+    console.log(`> aria2c -d /downloads --seed-time=0 ${target}`)
+    const aria2c = spawn('aria2c', [`-d /downloads --seed-time=0 ${target}`], {
+      shell: true
+    })
+    aria2c.stdout.on('data', (data) => {
+      console.log(data.toString())
+    })
+
+    aria2c.stderr.on('data', (data) => {
+      console.error(data.toString())
+      reject(false)
+    })
+
+    aria2c.on('close', (code) => {
+      console.log(`aria2 exited with code ${code}`)
+      resolve(true)
+    })
+  })
+
+const listFiles = (dir: string): string[] =>
+  fs.readdirSync(dir, { withFileTypes: true }).flatMap((dirent) => {
+    return dirent.isFile()
+      ? [`${dir}/${dirent.name}`]
+      : listFiles(`${dir}/${dirent.name}`)
+  })
+
+const upload = async (files: string[], code: string, sizeMin = 500000000) =>
+  Promise.all(
+    files
+      .filter((filePath) => fs.statSync(filePath).size > sizeMin)
+      .map((filePath, index) => {
+        const key = `/downloads/complete/001/${code}/${index + 1}${path.extname(
+          filePath
+        )}`
+        console.log('uploading: ', filePath, ' => ', key)
+        const command = new PutObjectCommand({
+          Bucket: 'transmission-project',
+          Key: key,
+          Body: fs.readFileSync(filePath)
+        })
+        return s3.send(command)
+      })
+  )
+
+const main = async () => {
+  const target = process.argv[2]
+  if (!target) throw new Error('pass torrent file link for args')
+
+  await aria(target)
+  const files = listFiles('/downloads')
+  const code = 'TEST-123'
+  await upload(files, code, 2000000)
+}
+
+main()
--- transmission/done.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-touch /downloads/complete/completed
