0ce830db5228d3ca3e9c1f982ebbe9befe36e7a1
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-25 00:29:11 +0900

    update(batch): update script

--- batch/script.ts
+++ batch/script.ts
@@ -7,10 +7,10 @@ const s3 = new S3Client({
   region: process.env.AWS_DEFAULT_REGION
 })
 
-const aria = async (target: string) =>
+const download = async (target: string, dir: string) =>
   new Promise((resolve, reject) => {
-    console.log(`> aria2c -d /downloads --seed-time=0 ${target}`)
-    const aria2c = spawn('aria2c', [`-d /downloads --seed-time=0 ${target}`], {
+    console.log(`> aria2c -d ${dir} --seed-time=0 ${target}`)
+    const aria2c = spawn('aria2c', [`-d ${dir} --seed-time=0 ${target}`], {
       shell: true
     })
     aria2c.stdout.on('data', (data) => {
@@ -35,32 +35,33 @@ const listFiles = (dir: string): string[] =>
       : listFiles(`${dir}/${dirent.name}`)
   })
 
-const upload = async (files: string[], code: string, sizeMin = 500000000) =>
+const upload = async (files: string[], code: string) =>
   Promise.all(
-    files
-      .filter((filePath) => fs.statSync(filePath).size > sizeMin)
-      .map((filePath, index) => {
-        const key = `${process.env.KEY_PREFIX}/${code}/${
-          index + 1
-        }${path.extname(filePath)}`
-        console.log('uploading: ', filePath, ' => ', key)
-        const command = new PutObjectCommand({
-          Bucket: process.env.BUCKET,
-          Key: key,
-          Body: fs.readFileSync(filePath)
-        })
-        return s3.send(command)
+    files.map((filePath, index) => {
+      const key = `${process.env.KEY_PREFIX}/${code}/${index + 1}${path.extname(
+        filePath
+      )}`
+      console.log('uploading: ', filePath, ' => ', key)
+      const command = new PutObjectCommand({
+        Bucket: process.env.BUCKET,
+        Key: key,
+        Body: fs.readFileSync(filePath)
       })
+      return s3.send(command)
+    })
   )
 
 const main = async () => {
   const target = process.argv[2]
   if (!target) throw new Error('pass torrent file link for args')
 
-  await aria(target)
-  const files = listFiles('/downloads')
-  const code = 'TEST-123'
-  await upload(files, code)
+  await download(target, '/downloads')
+  await upload(
+    listFiles('/downloads').filter(
+      (filePath) => fs.statSync(filePath).size > 500000000
+    ),
+    'TEST-123'
+  )
 }
 
 main()
