842f56c8e5ca796e8548c85a0e96a0fedd53240f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-27 21:47:14 +0900

    update(remix): update stocks

--- remix/app/routes/stocks.tsx
+++ remix/app/routes/stocks.tsx
@@ -10,6 +10,8 @@ type Data = {
     isLiked: boolean
     title: string
     mainImageUrl: string
+    idDownloaded: boolean
+    isProcessing: boolean
   }[]
   page: number
 }
@@ -37,27 +39,36 @@ const Stocks: VFC = () => {
 
   return (
     <>
-      {data.items.map(({ code, mainImageUrl, title }) => (
-        <div className="w-full flex mb-2" key={code}>
-          <Link
-            to={`/product/${code}`}
-            className="h-48 w-36 flex-none bg-contain bg-no-repeat text-center overflow-hidden"
-            style={{
-              backgroundImage: `url("${mainImageUrl}")`
-            }}
-          />
-          <div className="flex flex-col justify-between leading-normal">
-            <div className="mb-8">
-              <Link
-                to={`/product/${code}`}
-                className="text-gray-200 block text-sm mb-2"
-              >
-                {title.slice(0, 70)}
-              </Link>
+      {data.items.map(
+        ({ code, mainImageUrl, title, isProcessing, isDownloaded }) => (
+          <div className="w-full flex mb-2" key={code}>
+            <Link
+              to={`/product/${code}`}
+              className="h-48 w-36 flex-none bg-contain bg-no-repeat text-center overflow-hidden"
+              style={{
+                backgroundImage: `url("${mainImageUrl}")`
+              }}
+            />
+            <div className="flex flex-col justify-between leading-normal">
+              <div className="mb-8">
+                <Link
+                  to={`/product/${code}`}
+                  className="text-gray-200 block text-sm mb-2"
+                >
+                  {isProcessing ? (
+                    <span className="text-yellow-600 pr-1">●</span>
+                  ) : isDownloaded ? (
+                    <span className="text-green-500 pr-1">●</span>
+                  ) : (
+                    <span className="text-gray-500 pr-1">●</span>
+                  )}
+                  {title.slice(0, 70)}
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
       <div className="px-4 py-3 flex items-center justify-between border-t border-gray-300">
         <div className="flex-1 flex justify-between">
           <Link
