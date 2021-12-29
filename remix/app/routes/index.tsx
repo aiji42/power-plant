0faacf9b5711827d57716ce357af2f1aa8bf6225
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-30 00:25:25 +0900

    update(remix): update index for login

--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,100 +1,69 @@
-import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import { useActionData, Form, ActionFunction, redirect, json } from 'remix'
+import { supabaseClient } from '~/utils/supabase.server'
+import { commitSession, getSession } from '~/utils/session.server'
 
-type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
-};
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+  const email = form.get('email') as string
+  const password = form.get('password') as string
 
-// Loaders provide data to components and are only ever called on the server, so
-// you can connect to a database or run any server side code you want right next
-// to the component that renders it.
-// https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
-      {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
-      },
-      {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
-      },
-      {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
+  const { session: user, error } = await supabaseClient.auth.signIn({
+    email,
+    password
+  })
+
+  if (user) {
+    const session = await getSession(request.headers.get('Cookie'))
+    session.set('access_token', user.access_token)
+
+    return redirect('/news', {
+      headers: {
+        'Set-Cookie': await commitSession(session)
       }
-    ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
-  };
+    })
+  }
 
-  // https://remix.run/api/remix#json
-  return json(data);
-};
+  return json({ user, error: error?.message })
+}
 
-// https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
-  return {
-    title: "Remix Starter",
-    description: "Welcome to remix!"
-  };
-};
-
-// https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
+  const actionData = useActionData()
 
   return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
-        <h2>Demos In This App</h2>
-        <ul>
-          {data.demos.map(demo => (
-            <li key={demo.to} className="remix__page__resource">
-              <Link to={demo.to} prefetch="intent">
-                {demo.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map(resource => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="px-8 py-6 mt-4 text-left bg-gray-800 shadow-lg text-gray-200">
+        <h3 className="text-2xl font-bold text-center">
+          Login to your account
+        </h3>
+        <Form method="post">
+          <div className="mt-4">
+            <div>
+              <label className="block" htmlFor="email">
+                Email
+              </label>
+              <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                className="w-full px-4 py-2 mt-2 border rounded-md text-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-600"
+              />
+            </div>
+            <div className="mt-4">
+              <label className="block">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                className="w-full px-4 py-2 mt-2 border rounded-md text-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-600"
+              />
+            </div>
+            <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 w-full">
+              Login
+            </button>
+          </div>
+          <p className="text-gray-200">{actionData?.error}</p>
+        </Form>
+      </div>
     </div>
-  );
+  )
 }
