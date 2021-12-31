8f28934c69c76be7140c8463c0c0102ab3e23ae5
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-31 22:23:20 +0900

    update(remix): update root

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -1,6 +1,4 @@
 import {
-  ActionFunction,
-  Form,
   Link,
   Links,
   LiveReload,
@@ -10,13 +8,13 @@ import {
   redirect,
   Scripts,
   ScrollRestoration,
-  useActionData,
   useCatch,
-  useLoaderData
+  useLoaderData,
+  useLocation
 } from 'remix'
 import type { LinksFunction } from 'remix'
 import style from '~/tailwind.css'
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
 import { supabaseClient } from '~/utils/supabase.server'
 import { getSession } from '~/utils/session.server'
 
@@ -114,6 +112,7 @@ function Document({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
         {title ?? <title>{title}</title>}
         <Meta />
         <Links />
@@ -130,26 +129,33 @@ function Document({
 
 function Layout({ children }: { children: React.ReactNode }) {
   const [open, toggle] = useReducer((s) => !s, false)
+  const { user } = useLoaderData()
+  const href = useLocation()
+  useEffect(() => {
+    open && toggle()
+  }, [href.key])
   return (
     <div className="bg-gray-900">
       <header className="mb-2">
         <nav className="flex items-center justify-between flex-wrap p-4 border-b border-gray-500">
           <div className="flex items-center flex-shrink-0 text-white mr-6" />
-          <div className="block lg:hidden">
-            <button
-              onClick={toggle}
-              className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
-            >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+          {user && (
+            <div className="block">
+              <button
+                onClick={toggle}
+                className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white"
               >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+          )}
           {open && (
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
               <div className="text-sm lg:flex-grow">
