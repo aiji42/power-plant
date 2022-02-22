49ec77183d7981e491a85a250717794a6f033fe2
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-02-22 23:11:53 +0900

    update(remix): update header

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -156,23 +156,10 @@ const Layout: FC = ({ children }) => {
   }, [href.key])
   return (
     <div className="bg-gray-900 text-gray-200 min-h-screen">
-      <header className="mb-2">
+      <header className="sticky top-0 mb-2 bg-gray-900">
         <nav className="flex items-center justify-between flex-wrap p-2 px-4 border-b border-gray-500">
-          <div className="flex items-center flex-shrink-0 text-white mr-6" />
-          <div className="block">
-            <button
-              onClick={toggle}
-              className="flex items-center px-3 py-2 border rounded border-gray-400 active:text-white active:border-white"
-            >
-              <svg
-                className="fill-current h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
+          <div className="flex items-center m-auto text-xl" onClick={toggle}>
+            POWER PLANT
           </div>
           {open && (
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
