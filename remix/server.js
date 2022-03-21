f4741ba76b6102e0ea36aa51f2e717e4dd3f1832
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-03-21 18:46:10 +0900

    update(remix): server.js

--- remix/server.js
+++ remix/server.js
@@ -1,7 +1,15 @@
-import { createEventHandler } from "@remix-run/cloudflare-workers";
-import * as build from "@remix-run/dev/server-build";
+import { createEventHandler } from '@remix-run/cloudflare-workers'
+import * as build from '@remix-run/dev/server-build'
 
 addEventListener(
-  "fetch",
-  createEventHandler({ build, mode: process.env.NODE_ENV })
-);
+  'fetch',
+  createEventHandler({
+    build,
+    mode: process.env.NODE_ENV,
+    getLoadContext: (event) => {
+      const cache = caches.default
+
+      return { event, cache }
+    }
+  })
+)
