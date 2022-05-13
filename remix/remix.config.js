9eea9dcefe3bb8067a16902e7e2cb65ad3527475
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-05-14 01:47:32 +0900

    update(remix): use remix-service-bindings

--- remix/remix.config.js
+++ remix/remix.config.js
@@ -2,10 +2,12 @@ const { withEsbuildOverride } = require('remix-esbuild-override')
 const GlobalsPolyfills =
   require('@esbuild-plugins/node-globals-polyfill').default
 const alias = require('esbuild-plugin-alias')
+const remixServiceBindings = require('remix-service-bindings').default
 
 withEsbuildOverride((option, { isServer }) => {
   if (isServer)
     option.plugins = [
+      remixServiceBindings(!process.env.BINDEE, 'BINDEE', !!process.env.DEPLOY),
       alias({
         '@prisma/client': require.resolve('@prisma/client')
       }),
